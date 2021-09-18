3f774af44d198efd2678aeb0348d1a70e14ed0fb
yuriiihor <hefestsbunker@gmail.com>
2021-09-18 17:13:09 +0300

    Some changes

--- scripts/modules/config.js
+++ scripts/modules/config.js
@@ -18,3 +18,31 @@ const screen_width = 400
 const screen_height = 400
 const screen_border_width = 5
 const screen_border_color = "#000000"
+
+class Delegate {
+    listeners = [];
+
+    constructor() {
+        this.listeners = [];
+    }
+
+    addListener = function(newListener) {
+        this.listeners.push(newListener);
+    }
+
+    removeListener = function(listener) {
+        if(this.listeners.contains(listener)) {
+            this.listeners.remove(listener);
+        }
+    }
+
+    invoke = function(argument) {
+        if(this.listeners.length == 0) {
+            return;
+        }
+
+        for(let i = 0; i < this.listeners.length; i++) {
+            this.listeners[i](argument);
+        }
+    }
+}
