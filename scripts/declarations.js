fe207e33c0d7a9faf4f0c73cc0946f0357b8abcd
yuriiihor <hefestsbunker@gmail.com>
2021-09-19 01:18:56 +0300

    Removed config

--- scripts/declarations.js
+++ scripts/declarations.js
@@ -1,4 +1,14 @@
 
+const width = $(window).width()
+const height = $(window).height()
+
+const silhouette_padding = 60
+
+const screen_width = 400
+const screen_height = 400
+const screen_border_width = 5
+const screen_border_color = "#000000"
+
 const playerStats = {
     hitpoints: "Hitpoints",
     mana: "Mana",
@@ -15,7 +25,6 @@ const playerRating = {
     rank: 10,
     wonBattles: 10
 }
-
 class Skill {
     constructor (name, duration, effect, manaRequired, targetStat) {
         this.name = name;
