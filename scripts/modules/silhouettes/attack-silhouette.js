e9e7c6cc09b16b8e3eaec67f1e5181bf59097137
yuriiihor <hefestsbunker@gmail.com>
2021-09-22 16:55:13 +0300

    Bars text

--- scripts/modules/silhouettes/attack-silhouette.js
+++ scripts/modules/silhouettes/attack-silhouette.js
@@ -1,4 +1,27 @@
 
 class AttackSilhouette extends Silhouette {
-    
+
+    render(_context) {
+        this._context = _context;
+        
+        for (var body_part_name in this.coordinate_map) {
+            let body_part_image_type;
+            
+            if(body_part_name == this.hovering || body_part_name == this.selected_body_part) {
+                body_part_image_type = 'filled';
+            }
+            else {
+                body_part_image_type = 'hollow';
+            }
+
+            _context.drawImage(
+                this.body_parts_sprites[`${body_part_name}-${body_part_image_type}`],
+                this.x + this.coordinate_map[body_part_name]["left"] * this.scale,
+                this.y + this.coordinate_map[body_part_name]["top"] * this.scale,
+                this.coordinate_map[body_part_name]["width"] * this.scale,
+                this.coordinate_map[body_part_name]["height"] * this.scale
+            )
+            
+        }
+    }
 }
