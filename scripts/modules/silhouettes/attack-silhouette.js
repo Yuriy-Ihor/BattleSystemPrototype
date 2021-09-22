11108e11662fd4cbc04466c91535ba6a2ccdbb05
yuriiihor <hefestsbunker@gmail.com>
2021-09-22 23:56:49 +0300

    Buttons as inputs

--- scripts/modules/silhouettes/attack-silhouette.js
+++ scripts/modules/silhouettes/attack-silhouette.js
@@ -14,14 +14,19 @@ class AttackSilhouette extends Silhouette {
                 body_part_image_type = 'hollow';
             }
 
+            let width = this.coordinate_map[body_part_name]["width"] * this.scale;
+            let height = this.coordinate_map[body_part_name]["height"] * this.scale;
+            
+            //_context.fillStyle = "#0000";
             _context.drawImage(
                 this.body_parts_sprites[`${body_part_name}-${body_part_image_type}`],
                 this.x + this.coordinate_map[body_part_name]["left"] * this.scale,
                 this.y + this.coordinate_map[body_part_name]["top"] * this.scale,
-                this.coordinate_map[body_part_name]["width"] * this.scale,
-                this.coordinate_map[body_part_name]["height"] * this.scale
+                width, height
             )
-            
         }
+
+        // _context.fillStyle = "#555555";
+        // _context.fillText("Text", 10, 10);
     }
 }
