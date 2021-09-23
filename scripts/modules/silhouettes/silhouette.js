30125dea4c27ff1eaf254d3e43fce81dca7fb8c8
yuriiihor <hefestsbunker@gmail.com>
2021-09-23 15:46:16 +0300

    ; )

--- scripts/modules/silhouettes/silhouette.js
+++ scripts/modules/silhouettes/silhouette.js
@@ -9,14 +9,14 @@ const bodyPart = {
 
 class Silhouette {
     constructor(_x, _y, _size, _coordinate_map, canvas) {
-        this.x = _x
-        this.y = _y
-        this.scale = _size / _coordinate_map["unscaled-size"]
-        this.relevance = _coordinate_map["relevance"]
-        this.coordinate_map = {}
+        this.x = _x;
+        this.y = _y;
+        this.scale = _size / _coordinate_map["unscaled-size"];
+        this.relevance = _coordinate_map["relevance"];
+        this.coordinate_map = {};
         for (var body_part_name in _coordinate_map) {
             if (body_part_name != "unscaled-size" && body_part_name != "relevance") {
-                this.coordinate_map[body_part_name] = _coordinate_map[body_part_name]
+                this.coordinate_map[body_part_name] = _coordinate_map[body_part_name];
             }
         }
 
@@ -24,8 +24,8 @@ class Silhouette {
         this.body_parts = [];
         
         for (var body_part_name in this.coordinate_map) {
-            this.body_parts_sprites[`${body_part_name}-hollow`] = document.getElementById(`${body_part_name}-hollow-${this.relevance}`)
-            this.body_parts_sprites[`${body_part_name}-filled`] = document.getElementById(`${body_part_name}-filled-${this.relevance}`) 
+            this.body_parts_sprites[`${body_part_name}-hollow`] = document.getElementById(`${body_part_name}-hollow-${this.relevance}`);
+            this.body_parts_sprites[`${body_part_name}-filled`] = document.getElementById(`${body_part_name}-filled-${this.relevance}`);
             
             let newBodyPart = Object.create(bodyPart);
             newBodyPart.id = body_part_name;
@@ -33,14 +33,14 @@ class Silhouette {
             this.body_parts[body_part_name] = newBodyPart;
         }
 
-        this.hovering = null
+        this.hovering = null;
 
         canvas.addEventListener(
             "mousemove",
             (event) => {
                 if(this._context != null) {
                     if(isElementParentVisible(this._context.canvas)) {
-                        this.check_mouseover(event.offsetX, event.offsetY)
+                        this.check_mouseover(event.offsetX, event.offsetY);
                     }
                 }
             }
@@ -72,10 +72,10 @@ class Silhouette {
                 body_part_Y < _y &&
                 _y < body_part_Y + this.get_silhouette_coordinate(body_part_name, 0, 'height')
             ) {
-                this.hovering = body_part_name
-                break
+                this.hovering = body_part_name;
+                break;
             } else {
-                this.hovering = null
+                this.hovering = null;
             }
         }
     }
@@ -102,7 +102,7 @@ class Silhouette {
                 this.y + this.coordinate_map[body_part_name]["top"] * this.scale,
                 this.coordinate_map[body_part_name]["width"] * this.scale,
                 this.coordinate_map[body_part_name]["height"] * this.scale
-            )
+            );
         }
     }
 }
