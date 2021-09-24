4510ce039cb3bd8fe52e97c707d6e9a8f2080afd
yuriiihor <hefestsbunker@gmail.com>
2021-09-25 00:34:08 +0300

    Minor changes

--- scripts/silhouettes/silhouette.js
+++ scripts/silhouettes/silhouette.js
@@ -34,43 +34,15 @@ class Silhouette{
             newImage.setAttribute('y', positionY);
             newImage.setAttribute('width', this.coordinate_map[body_part_name]["width"]);
             newImage.setAttribute('height', this.coordinate_map[body_part_name]["height"]);
-
-            newImage.addEventListener('mouseover', () => {
-                this.fillImage(newImage);
-            });
-
-            newImage.addEventListener('mouseout', () => {
-                if(this.selected != newImage) {
-                    this.hollowImage(newImage);
-                }
-            });
-
-            newImage.addEventListener('mousedown', () => {
-                if(this.selected != null) {
-                    this.hollowImage(this.selected);
-                }
-                this.selected = newImage;
-                this.fillImage(this.selected);
-            });
             
             display.appendChild(newImage);
         }
 
-        this.selected = null;
         this.display = display;
     }
 
     getImagesPath(type, id) {
         return `${silhouetteImagePath}/${this.relevance}/${type}/${id}-${type}.png`;
-    }
-
-    isBodyPartSelected() {
-        return this.selected != null;
-    }
-
-    disselectBodyPart() {
-        this.hollowImage(this.selected);
-        this.selected = null;
     }
 
     hollowImage(image) { 
@@ -80,20 +52,15 @@ class Silhouette{
     fillImage(image) {
         image.setAttribute('href', this.getImagesPath('filled', image.id));
     }
+
+    render() {
+        // todo : update position on resize
+    }
 }
 
 class SelectableSilhouette extends Silhouette {
     constructor(_coordinate_map, display) {
         super(_coordinate_map, display);
-        this.relevance = _coordinate_map["relevance"];
-        this.size = _coordinate_map['unscaled-size'];
-        this.coordinate_map = {};
-
-        for (var body_part_name in _coordinate_map) {
-            if (body_part_name != "unscaled-size" && body_part_name != "relevance") {
-                this.coordinate_map[body_part_name] = _coordinate_map[body_part_name];
-            }
-        }
 
         for (var body_part_name in this.coordinate_map) {
             let positionX = this.coordinate_map[body_part_name]["left"] + (screenWidth - this.size) / 2;
@@ -131,11 +98,6 @@ class SelectableSilhouette extends Silhouette {
         }
 
         this.selected = null;
-        this.display = display;
-    }
-
-    getImagesPath(type, id) {
-        return `${silhouetteImagePath}/${this.relevance}/${type}/${id}-${type}.png`;
     }
 
     isBodyPartSelected() {
@@ -147,15 +109,4 @@ class SelectableSilhouette extends Silhouette {
         this.selected = null;
     }
 
-    hollowImage(image) { 
-        image.setAttribute('href', this.getImagesPath('hollow', image.id));
-    }
-
-    fillImage(image) {
-        image.setAttribute('href', this.getImagesPath('filled', image.id));
-    }
- 
-    render() {
-        // todo : update position on resize
-    }
 }
