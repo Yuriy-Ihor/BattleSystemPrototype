62d2a7447053c292cc06672e9e5163d013816277
yuriiihor <hefestsbunker@gmail.com>
2021-09-26 21:36:06 +0300

    Better silhouette scaling

--- scripts/game-config.js
+++ scripts/game-config.js
@@ -5,4 +5,6 @@ const BODY_PART_LIFE = 4;
 const ATTACKED_ENEMY_PART_ID = 'head';
 const DEFENDED_ENEMY_PART_ID = 'torso';
 
-const UI_SCALE = 2;
+const SILHOUETTE_SIZE = 256; 
+const UI_SCALE_MULTIPLAYER = 0.45;
+const UI_SCALE = UI_SCALE_MULTIPLAYER * Math.min(window.innerHeight, window.innerWidth) / SILHOUETTE_SIZE;
