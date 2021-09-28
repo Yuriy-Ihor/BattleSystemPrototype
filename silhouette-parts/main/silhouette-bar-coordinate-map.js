6b19d1c92affeaed59fcbc5051774c9531473705
yuriiihor <hefestsbunker@gmail.com>
2021-09-28 23:09:05 +0300

    Fixed silhouettes

--- silhouette-parts/main/silhouette-bar-coordinate-map.js
+++ silhouette-parts/main/silhouette-bar-coordinate-map.js
@@ -1,21 +1,21 @@
 silhouette_bar_coordinate_map_main = {
     "relevance": "main",
     "unscaled-size": 256,
-    "arm-left": {
+    "arm-right": {
         "left": 170,
         "top": 80
     },
-    "arm-right": {
+    "arm-left": {
         "left": 80,
         "top": 90
     },
     "leg-right": {
-        "left": 90,
-        "top": 180
-    },
-    "leg-left": {
         "left": 175,
         "top": 160
+    },
+    "leg-left": {
+        "left": 90,
+        "top": 180
     },
     "head": {
         "left": 115,
