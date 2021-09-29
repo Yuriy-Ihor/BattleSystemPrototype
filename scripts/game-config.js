9fe5b7a4aeb89438b63d07ab38df3d564a074899
yuriiihor <hefestsbunker@gmail.com>
2021-09-29 14:21:14 +0300

    Fixes with summary silhouettes

--- scripts/game-config.js
+++ scripts/game-config.js
@@ -6,10 +6,12 @@ const ATTACKED_ENEMY_PART_ID = 'head';
 const DEFENDED_ENEMY_PART_ID = 'torso';
 
 const SILHOUETTE_SIZE = 256; 
-const SUMMARY_SILHOUETTE_SIZE = 160; 
+const SUMMARY_SILHOUETTE_SIZE = 1000; 
 
 const UI_SCALE_MULTIPLAYER = 0.45;
 const MAX_UI_SCALE = 1.5;
+
+const MINIMAL_SCREEN_SIZE = Math.min(window.innerHeight, window.innerWidth);
 
 var UI_SCALE = getUiScale();
 
@@ -21,16 +23,5 @@ function getUiScale() {
     }
     else {
         return Math.min((minimalSize / SILHOUETTE_SIZE), MAX_UI_SCALE);
-    }
-}
-
-function getSizeScaled(size) {
-    let minimalSize = Math.min(window.innerHeight, window.innerWidth);
-
-    if(size > minimalSize) {
-        return UI_SCALE_MULTIPLAYER *  minimalSize / size;
-    }
-    else {
-        return Math.min((minimalSize / size), MAX_UI_SCALE);
     }
 }
